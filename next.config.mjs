20232024e1c6ac88dbe5e8a2fed0218c7e3e05cf
saufth <saufth@gmail.com>
2025-05-21 22:45:34 -0600

    move env impoort on next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import('./src/env.mjs')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true
   }
 }
+
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import('./src/env.mjs')
 
 export default nextConfig
