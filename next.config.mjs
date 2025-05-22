73021d4ecf37e9e517f240d3e14ac0f0472cbc21
saufth <saufth@gmail.com>
2025-05-21 22:46:32 -0600

    remove env impoort on next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,4 @@ const nextConfig = {
   }
 }
 
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-await import('./src/env.mjs')
-
 export default nextConfig
